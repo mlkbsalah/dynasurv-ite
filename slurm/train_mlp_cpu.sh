02a548352a4bcfeb164b8c0511b1a23a194242a2
Malek Ben Salah <malek.ben-salah@centralesupelec.fr>
2025-12-19 14:30:18 +0100

    slurm scripts

---
+++ slurm/train_mlp_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=dynasurvonline_training
+#SBATCH --output=%x.o%j
+#SBATCH --time=24:00:00 
+#SBATCH --partition=gpua100
+
+module load anaconda3/2024.06/gcc-13.2.0
+module load cuda/12.8.0/gcc-13.2.0
+
+source activate pytorch_env 
+
+PROJECT_DIR="/workdir/bensalama/DynaSurv"
+cd "$PROJECT_DIR/scripts"
+echo $PWD
+
+export PYTHONPATH="$PROJECT_DIR/src"
+
+python3 train_DynaSurv_sweep.py
