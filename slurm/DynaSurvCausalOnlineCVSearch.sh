0d5debbc49232fadd50fe90571f7b534284539b2
Malek Ben Salah <malek.ben-salah@centralesupelec.fr>
2026-01-13 14:44:53 +0100

    Add automation

---
+++ slurm/DynaSurvCausalOnlineCVSearch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=DynaSurvCausalOnlineCV
+#SBATCH --output=logs/%x.o%j
+#SBATCH --time=5:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpu
+
+# Module and env setup
+module load anaconda3/2024.06/gcc-13.2.0
+module load cuda/12.8.0/gcc-13.2.0
+
+source activate pytorch_env 
+
+PROJECT_DIR="/workdir/bensalama/DynaSurv"
+cd "$PROJECT_DIR/scripts"
+
+export PYTHONPATH="$PROJECT_DIR/src"
+
+# Script
+python3 DynaSurvCausalOnlineCVSearch.py \
+        --trial_id ${SLURM_ARRAY_TASK_ID} \
+        --split_seed $SPLIT_SEED \
+        --n_folds $N_FOLDS \
+        --project_name $PROJECT_NAME
---
+++ slurm/LaunchCVSearch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+N_TRIALS=1
+
+# Parse flags
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--n_trials)
+      N_TRIALS="$2"
+      shift 2
+      ;;
+    -f|--n_folds)
+      N_FOLDS="$2"
+      shift 2
+      ;;
+    -s|--split_seed)
+      SPLIT_SEED="$2"
+      shift 2
+      ;;
+    -p|--project_name)
+      PROJECT_NAME="$2"
+      shift 2
+      ;;
+    -h|--help)
+      echo "Usage: ./launch [options]"
+      echo "Options:"
+      echo "  -t, --n_trials      Number of trials (default: 1)"
+      echo "  -f, --n_folds       Number of folds (required)"
+      echo "  -s, --split_seed    Split seed (required)"
+      echo "  -p, --project_name  Project name"
+      exit 0
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
+
+#Arguments parsing
+if [[ -z "$N_FOLDS" || -z "$SPLIT_SEED" ]]; then
+	echo "Usage: sbatch script.sh [n_trials] [n_folds] [split_seed] [project_name]"
+	echo "Error: Both n_folds and split_seed are required arguments"
+	exit 1
+fi
+
+if [[ -z "$PROJECT_NAME" ]]; then 
+	echo "Warning: unspecified project name, logging disabled"
+fi
+
+
+sbatch --parsable --export=ALL,N_FOLDS=$N_FOLDS,SPLIT_SEED=$SPLIT_SEED,PROJECT_NAME=$PROJECT_NAME --array=0-$((N_TRIALS-1)) DynaSurvCausalOnlineCVSearch.sh
